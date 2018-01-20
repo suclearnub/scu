3251a32e966915a66187b31d8e8e8642e38d8ae2
Anson <git@anson-cheung.com>
2018-01-20 21:58:51 +0800

    Fix multiple issues

--- crypto/price.py
+++ crypto/price.py
@@ -18,7 +18,7 @@ class Price(BotModule):
     module_version = '0.1.0'
 
     def price_change(self, percent, price):
-        return (price)/(1+(percent/100))
+        return (float(price))/(1+(float(percent)/100))
 
     async def parse_command(self, message, client):
         msg = shlex.split(message.content)
@@ -28,7 +28,7 @@ class Price(BotModule):
             embed = discord.Embed(title= data["name"] + " Information", description="#" + data["rank"] + " in terms of market capitalization")
             embed.add_field(name="Price", value="$ " + data["price_usd"] + "USD", inline=True)
             embed.add_field(name="24h Change", value=data["percent_change_24h"] + "% (" + self.price_change(data["percent_change_24h"], data["price_usd"]) + ")", inline=True)
-            embed.add_field(name="Market Capitalization", value="US$" + "{:,}".format(data["market_cap_usd"]), inline=True)
-            embed.add_field(name="24h Volume", value="US$" + "{:,}".format(data["24h_volume_usd"]), inline=True)
+            embed.add_field(name="Market Capitalization", value="US$" + "{:,}".format(int(data["market_cap_usd"])), inline=True)
+            embed.add_field(name="24h Volume", value="US$" + "{:,}".format(int(data["24h_volume_usd"])), inline=True)
             embed.set_footer(text="Information provided by coinmarketcap.com")
             await client.send_message(message.channel, embed=embed)
