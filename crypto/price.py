07e7235b248495e042ae43abb2809012edaa7157
Anson <git@anson-cheung.com>
2018-01-20 22:05:13 +0800

    Fix formatting issues

--- crypto/price.py
+++ crypto/price.py
@@ -18,7 +18,7 @@ class Price(BotModule):
     module_version = '0.1.0'
 
     def price_change(self, percent, price):
-        return str((float(price))/(1+(float(percent)/100)))
+        return "{0:.2f}".format((float(price))/(1+(float(percent)/100)))
 
     async def parse_command(self, message, client):
         msg = shlex.split(message.content)
@@ -26,9 +26,9 @@ class Price(BotModule):
             html = requests.get("https://api.coinmarketcap.com/v1/ticker/" + msg[1])
             data = html.json()[0]
             embed = discord.Embed(title= data["name"] + " Information", description="#" + data["rank"] + " in terms of market capitalization")
-            embed.add_field(name="Price", value="$ " + data["price_usd"] + "USD", inline=True)
+            embed.add_field(name="Price", value="US$ " + data["price_usd"], inline=True)
             embed.add_field(name="24h Change", value=data["percent_change_24h"] + "% (" + self.price_change(data["percent_change_24h"], data["price_usd"]) + ")", inline=True)
-            embed.add_field(name="Market Capitalization", value="US$" + "{:,}".format(int(data["market_cap_usd"])), inline=True)
-            embed.add_field(name="24h Volume", value="US$" + "{:,}".format(int(data["24h_volume_usd"][:-2])), inline=True)
+            embed.add_field(name="Market Capitalization", value="US$ " + "{:,}".format(int(data["market_cap_usd"])), inline=True)
+            embed.add_field(name="24h Volume", value="US$ " + "{:,}".format(int(data["24h_volume_usd"][:-2])), inline=True)
             embed.set_footer(text="Information provided by coinmarketcap.com")
             await client.send_message(message.channel, embed=embed)
