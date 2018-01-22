4fcd9655b13edca302c13082036919da4059c934
Anson <git@anson-cheung.com>
2018-01-22 11:28:59 +0800

    Remove baked-in resolver

--- crypto/enslookup.py
+++ crypto/enslookup.py
@@ -2,11 +2,19 @@ import requests
 from modules.crypto.namehash import namehash
 import binascii
 
-def data(nh):
-    b = binascii.b2a_hex(namehash(nh))
+ens_registry = "0x314159265dD8dbb310642f98f50C066173C1259b"
+
+def data_registrar(addr):
+    b = binascii.b2a_hex(namehash(addr))
+    return '0x0178b8bf' + b.decode("unicode_escape")
+
+def data_resolver(addr):
+    b = binascii.b2a_hex(namehash(addr))
     return '0x3b3b57de' + b.decode("unicode_escape")
 
 def ENSLookup(address):
-    html = requests.get("https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x5FfC014343cd971B7eb70732021E26C35B744cc4&data="
-                        + data(address) + "&tag=latest&apikey=YourApiKeyToken")
+    resolver_address = requests.get("https://api.etherscan.io/api?module=proxy&action=eth_call&to=" + ens_registry + "&data="
+                        + data_registrar(address) + "&tag=latest&apikey=YourApiKeyToken").json()["result"]
+    html = requests.get("https://api.etherscan.io/api?module=proxy&action=eth_call&to=" + resolver_address + "&data="
+                        + data_resolver(address) + "&tag=latest&apikey=YourApiKeyToken")
     return html.json()["result"]
